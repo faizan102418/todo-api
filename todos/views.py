3e3a03bda16bb2c073bc6d1d4b9a433948ae6eaa
faizan102418 <m.faizansajid102418@gmail.com>
2026-06-15 10:47:07 +0500

    feat: add filtering by completed and due_date

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Todo
 from .serializers import TodoSerializer
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['completed', 'due_date']
